0266044120b62fd3b2aa9dc41b2863ee06a719c9
Miroslav Tsarev <mtzarev@gmail.com>
2024-08-20 18:49:29 +0300

    Add files via upload

---
+++ 3. Town Info.js
@@ -0,0 +1,15 @@
+function townInfo(townName, population, area) {
+    if (townName.length < 3) {
+        console.log(`Town name must be at least 3 characters!`);
+    }
+    if (population < 0) {
+        console.log(`Population must be a positive number!`);
+    }
+    if (area <= 0) {
+        console.log(`Area must be a positive number!`);
+    }
+    if (townName.length > 2 && population >= 0 && area > 0) {
+        console.log(`Town ${townName} has population of ${population} and area ${area} square km.`);
+    }
+}
+townInfo('sofiq', 1286383, 492);
